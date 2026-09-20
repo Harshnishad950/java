238b8fef8b585efd781a0c71cf3f2e294cbad707
Harshnishad950 <harshnishad950@gmail.com>
2026-09-20 17:44:35 +0530

    aaj to kuch accha hua

---
+++ code_forces/Ambitious_Kid.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+//1866A
+
+public class Ambitious_Kid {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        int min = Integer.MAX_VALUE;
+        for(int x : arr){
+            min = Math.min(min, Math.abs(x));
+        }
+        System.out.println(min);
+    }
+}
